4476f0795ebc4d28c7baba4c30f2caa408d2585d
Bhanu Prasad Nampally <jayajayaanth@gmail.com>
2025-06-13 17:04:40 +0530

    Fix Vite base path to resolve Netlify blank page

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/project_3D_developer_portfolio/',  // your GitHub repo name with slashes
-  plugins: [react()]
+  base: './',
+  plugins: [react()],
 })

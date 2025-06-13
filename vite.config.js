2337c5b2b6f02ee81c29e40298c7c155c4fa12bb
Bhanu Prasad Nampally <jayajayaanth@gmail.com>
2025-06-13 16:58:54 +0530

    Replaced react-tilt with react-parallax-tilt for compatibility with React 18

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/project_3D_developer_portfolio/',  // your GitHub repo name with slashes
+  plugins: [react()]
 })
